1fb43f7ed9fcdbce9b9a4b97e063e28c08f7c67c
FernandoA <fernandoam_1019@hotmail.com>
2017-10-30 18:52:47 -0600

    Creacion de Clases

---
+++ Control/src/Clases/Cliente.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Clases;
+
+/**
+ *
+ * @author Fernando Ambrosio
+ */
+public class Cliente {
+    private String nombre;
+    private String apellido;
+    private String dpi;
+    private String nit;
+    private int telefono;
+    private int celular;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(String dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getNit() {
+        return nit;
+    }
+
+    public void setNit(String nit) {
+        this.nit = nit;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+    
+    
+    
+    
+}
