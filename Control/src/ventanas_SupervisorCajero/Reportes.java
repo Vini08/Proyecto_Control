8ec1576f6ab5c2a70ff84931a9035f42e1d54deb
vini88 <vic_893@hotmail.com>
2017-10-30 21:28:21 -0600

    nuevas ventanas

--- Control/src/ventanas_SupervisorCajero/Reportes.java
+++ Control/src/ventanas_SupervisorCajero/Reportes.java
@@ -55,7 +55,6 @@ repaint();
         jLabel2 = new javax.swing.JLabel();
         jLabel10 = new javax.swing.JLabel();
         jButton2 = new javax.swing.JButton();
-        jDateChooser1 = new com.toedter.calendar.JDateChooser();
 
         setBackground(new java.awt.Color(204, 204, 204));
         setBorder(javax.swing.BorderFactory.createEmptyBorder(0, 0, 0, 0));
@@ -105,9 +104,6 @@ repaint();
         });
         getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 210, 310, 57));
 
-        jDateChooser1.setFont(new java.awt.Font("Dialog", 0, 22)); // NOI18N
-        getContentPane().add(jDateChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 140, 310, 50));
-
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
@@ -136,7 +132,6 @@ repaint();
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton2;
-    private com.toedter.calendar.JDateChooser jDateChooser1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
